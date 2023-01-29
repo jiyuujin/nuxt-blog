1d7b1f963332a0dccd5937cfea913f00c8417bb6
yuma-kitamura <jiyuujinunite@gmail.com>
2023-01-29 22:45:34 +0900

    feat: CssResetButton

--- packages/vue-ui/components/Button.vue
+++ packages/vue-ui/components/Button.vue
@@ -1,13 +1,21 @@
 <template>
-  <button @click="handleClick">Click Me</button>
+  <CssResetButton @click="handleClick">
+    <slot />
+  </CssResetButton>
 </template>
 
-<script lang="ts">
-export default {
-  methods: {
-    handleClick: () => {
-      alert("Woooha!");
-    },
-  },
-};
+<script setup lang="ts">
+import Vue from 'vue'
+import CssResetButton from './CssResetButton.vue'
+
+type ButtonProps = Vue.ButtonHTMLAttributes
+const props = defineProps<ButtonProps>()
+
+function handleClick() {
+  alert("Woooha!")
+}
 </script>
+
+<style lang="scss" scoped>
+@import './Button.styles.scss';
+</style>
---
+++ packages/vue-ui/components/CssResetButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button @click="handleClick">
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+import Vue from 'vue'
+
+type CssResetButtonProps = Vue.ButtonHTMLAttributes
+const props = defineProps<CssResetButtonProps>()
+
+function handleClick() {
+  alert("Woooha!")
+}
+</script>
+
+<style lang="scss" scoped>
+@import './CssResetBurron.styles.scss';
+</style>
